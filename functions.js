adb87e318c6740b9b91a84c15e3dd01f8409fa5f
Niko <niko@ibakoo.com>
2022-01-26 17:20:43 -0500

    modal

---
+++ functions.js
@@ -0,0 +1,62 @@
+var counter = 1;
+function AddNote()
+{
+    //vars
+    let noteContainer = document.getElementById('note-container');
+    let textInp = document.getElementById('inp').value;
+    let individualNotes = document.createElement('div');
+    let learnButton = document.createElement('button');
+    let parag = document.createElement('p');
+    let header = document.createElement('div');
+    let buttonDiv = document.createElement('div');
+    let par_modal = document.getElementById('p-modal');
+    let close = document.getElementsByClassName('close');
+
+
+    //styles
+    individualNotes.style.width ="600px";
+    individualNotes.style.height ="200px";
+    //classes
+    learnButton.classList.add("learn-more");
+    individualNotes.classList.add("indNotes")
+    parag.classList.add("parag");
+    header.classList.add("header-notes");
+    buttonDiv.classList.add("button-div");
+
+    //inerith
+    parag.innerText = textInp;
+    par_modal.innerText = textInp;
+    header.innerText = "#" + counter + ") Note:"
+    learnButton.innerText = "Learn More"
+
+   //check for empty text, if true don't show learn more
+    if(textInp === ""){ buttonDiv.style.display="none";}
+
+
+    //append
+    //
+    individualNotes.appendChild(header);
+    buttonDiv.appendChild(learnButton);
+    individualNotes.appendChild(parag);
+    individualNotes.appendChild(buttonDiv);
+
+
+    noteContainer.appendChild(individualNotes);
+    counter ++;
+    
+
+}
+
+
+
+// function closeSpan(e){
+//     var modal = document.getElementsByClassName("modal")
+//     var item = e.target; 
+//     if(item.className[0] === "close")
+//     {
+//         modal.style.display="none";
+//     }
+//     console.log("works")
+    
+// }
+
